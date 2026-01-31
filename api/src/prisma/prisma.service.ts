d3afa2a28b5067b65206be4b64ec2232967b54e6
tigrbn <asutaiiiga@gmail.com>
2026-02-01 00:49:44 +0900

    update prisma config

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import * as Prisma from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends Prisma.PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     const connectionString = process.env.DATABASE_URL;
     if (!connectionString) {
@@ -13,9 +16,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
 
     const adapter = new PrismaPg({ connectionString });
 
-    super({
-      adapter,
-    });
+    super({ adapter } as any);
   }
 
   async onModuleInit() {
